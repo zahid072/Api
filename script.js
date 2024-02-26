920b4b65501f0473dcd2fa98ea8bf28590658613
zahid072 <jahidhasan19368@gmail.com>
2024-02-26 23:32:01 +0600

    load user

--- script.js
+++ script.js
@@ -7,10 +7,33 @@ fetch("https://jsonplaceholder.typicode.com/posts")
       let div1 = document.createElement("div");
       div1.classList.add('container');
       div1.innerHTML = `
-         <p>${post.id}</p>
+         <p>Post: ${post.id}</p>
          <h1 class="text-4xl font-bold">${post.title}</h1>
          <p>${post.body}</p>
       `;
       content.appendChild(div1);
     }
   });
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    
+    const content = document.getElementById("user");
+    for (let user of data) {
+      let div1 = document.createElement("div");
+      div1.classList.add('container');
+      div1.innerHTML = `
+         <p>user: ${user.id}</p>
+         <h1 class="text-4xl font-bold">Name: ${user.name}</h1>
+         <p>Email: ${user.email}</p>
+         <p>City: ${user.address.city}</p>
+         <p>street: ${user.address.street}</p>
+      `;
+      content.appendChild(div1);
+    }
+  });
+
+
+
+
+
